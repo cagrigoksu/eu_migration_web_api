ee23944757cd3379dfe0ade763c0c50f94e3b43b
Cagri Goksu Ustundag <cagrigoksu.ustundag@gmail.com>
2025-02-10 23:50:37 +0100

    Added Immigration Proportion by Country and Stacked Bar Chart for Immigration & Emigration Trends

--- routes/data/analytics.py
+++ routes/data/analytics.py
@@ -36,6 +36,23 @@ def dashboard():
     fig_bar.add_trace(go.Bar(x=df['Year'], y=df['Em_Value'], name='Emigration'))
     fig_bar.update_layout(barmode='group', title='Immigration vs Emigration')
     bar_chart = fig_bar.to_html(full_html=False)
+    
+     # Immigration Proportion by Country (pie chart)
+    df_immigration = df.groupby('Country')['Im_Value'].sum().reset_index()
+    fig_pie = px.pie(
+        df_immigration,
+        names='Country',
+        values='Im_Value',
+        title='Proportion of Immigration by Country'
+    )
+    pie_chart = fig_pie.to_html(full_html=False)
+
+    # Stacked Bar Chart for Immigration & Emigration Trends 
+    fig_stacked = go.Figure()
+    fig_stacked.add_trace(go.Bar(x=df['Year'], y=df['Im_Value'], name='Immigration'))
+    fig_stacked.add_trace(go.Bar(x=df['Year'], y=df['Em_Value'], name='Emigration'))
+    fig_stacked.update_layout(barmode='stack', title='Stacked Immigration & Emigration Trends')
+    stacked_chart = fig_stacked.to_html(full_html=False)
 
     html_template = """
     <html>
@@ -46,27 +63,42 @@ def dashboard():
                 display: flex;
                 justify-content: space-around;
                 flex-wrap: wrap;
+                margin-top: 1%;
             }
             .chart-box {
                 width: 48%;
                 margin: 1%;
-                 min-height: 520px;
+                min-height: 520px;
             }
         </style>
     </head>
     <body>
         <h1 style='text-align:center;'>EU Migration Data Dashboard</h1>
+        <div>
+            <div class="chart-container">
+            
+                <div class="chart-box">
+                    <h2>Net Migration Trends</h2>
+                    <div>{{ line_chart|safe }}</div>
+                </div>
 
-        <div class="chart-container">
-        
-            <div class="chart-box">
-                <h2>Net Migration Trends</h2>
-                <div>{{ line_chart|safe }}</div>
+                <div class="chart-box">
+                    <h2>Immigration vs Emigration</h2>
+                    <div>{{ bar_chart|safe }}</div>
+                </div>
             </div>
+            
+            <div class="chart-container">
+            
+                <div class="chart-box">
+                    <h2>Immigration Proportion by Country</h2>
+                    <div>{{ pie_chart|safe }}</div>
+                </div>
 
-            <div class="chart-box">
-                <h2>Immigration vs Emigration</h2>
-                <div>{{ bar_chart|safe }}</div>
+                <div class="chart-box">
+                    <h2>Stacked Bar Chart for Immigration & Emigration Trends</h2>
+                    <div>{{ stacked_chart|safe }}</div>
+                </div>
             </div>
         </div>
 
@@ -78,4 +110,6 @@ def dashboard():
         html_template,
         line_chart=line_chart,
         bar_chart=bar_chart,
+        pie_chart=pie_chart,
+        stacked_chart=stacked_chart
     )
