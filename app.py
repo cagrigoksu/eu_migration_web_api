c50c4cf3930ad503252df648a26dd4dc2efda58c
Cagri Goksu Ustundag <cagrigoksu.ustundag@gmail.com>
2025-03-01 19:02:58 +0100

    major: fully working CI/CD pipeline..

--- app.py
+++ app.py
@@ -44,4 +44,4 @@ def main():
     return 'Europe Immigration/Emigration Analysis on AWS EC2'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080) # Bind to all network interfaces
+    app.run(host='0.0.0.0', port=8080) 
